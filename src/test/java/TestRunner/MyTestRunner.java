20d1fec6f80b404f2c3f240430036398b32cad80
FrameworkSeleniumPriya <seleniumpracticepriya@gmail.com>
2024-12-31 00:37:11 +0530

    Update MyTestRunner.java

--- src/test/java/TestRunner/MyTestRunner.java
+++ src/test/java/TestRunner/MyTestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/appFeatures", glue = {"com.stepDefinition", "AppHooks"},tags="@smoke",
-        plugin = {"pretty"
+        plugin = {"pretty",""io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         })
 public class MyTestRunner extends AbstractTestNGCucumberTests {
 }
